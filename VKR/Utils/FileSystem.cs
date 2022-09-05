ee9798ca8688ee38b26d78e4d989ece815170f82
DDoS <ermakov123321123321@mail.ru>
2022-09-05 12:39:46 +0300

    Вывод справки и начало создание файловой системы

---
+++ VKR/Utils/FileSystem.cs
@@ -0,0 +1,159 @@
+﻿//using iText.IO.Font;
+//using iText.IO.Image;
+//using iText.Kernel.Font;
+//using iText.Kernel.Geom;
+//using iText.Kernel.Pdf;
+//using iText.Layout;
+//using iText.Layout.Element;
+//using iText.Layout.Properties;
+
+//namespace VKR.Utils
+//{
+
+//    /// <summary>
+//    ///     Класс для работы с файлами
+//    /// </summary>
+//    internal static class FileSystem
+//    {
+//        private static Image CreateAndFitImage(byte[] bitmap, Document document)
+//        {
+//            var image = new Image(ImageDataFactory.Create(bitmap)).SetTextAlignment(TextAlignment.CENTER);
+//            FitImageToDocument(image, document);
+
+//            return image;
+//        }
+
+//        private static void FitImageToDocument(Image image, Document document)
+//        {
+//            var widthscaler =
+//                (document.GetPageEffectiveArea(PageSize.A4).GetWidth() - document.GetLeftMargin() -
+//                 document.GetRightMargin()) / image.GetImageWidth();
+
+//            var heighscaler =
+//                (document.GetPageEffectiveArea(PageSize.A4).GetHeight() - document.GetTopMargin() -
+//                 document.GetBottomMargin()) / image.GetImageHeight();
+
+//            float scaler;
+
+//            if (widthscaler < heighscaler)
+//            {
+//                scaler = widthscaler;
+//            }
+//            else
+//            {
+//                scaler = heighscaler;
+//            }
+
+//            image.Scale(scaler, scaler);
+//        }
+
+//        /// <summary>
+//        ///     Функция экспорта результатов в пдф
+//        /// </summary>
+//        /// <param name="path">Путь к файлу</param>
+//        /// <param name="tempBitmap">График температуры</param>
+//        /// <param name="nBitMap">График вязкости</param>
+//        /// <param name="mathModel">Результаты расчетов и начальные параметры</param>
+//        public static void ExportPdf(string path, byte[] tempBitmap, byte[] nBitMap, MathClass mathModel) // todo Переписать
+//        {
+//            var results = mathModel.Results;
+//            var writer = new PdfWriter(path);
+//            var pdf = new PdfDocument(writer);
+//            var document = new Document(pdf);
+
+//            //Trace.WriteLine($"-----------------------------------{Directory.GetCurrentDirectory()}");
+//            var fontFilename = "../../../resources/fonts/Times_New_Roman.ttf";
+//            var font = PdfFontFactory.CreateFont(fontFilename, PdfEncodings.IDENTITY_H);
+
+//            var header = new Paragraph("Отчёт о моделировании неизотермического течения аномально-вязкого материала").SetFont(font)
+//                .SetTextAlignment(TextAlignment.CENTER)
+//                .SetFontSize(20);
+
+//            document.SetFont(font);
+
+//            var tGraphImage = CreateAndFitImage(tempBitmap, document);
+//            document.Add(header);
+//            document.Add(new Paragraph("Входные данные"));
+
+//            var initialTable = new Table(UnitValue.CreatePercentArray(2)).UseAllAvailableWidth();
+
+//            initialTable.AddCell(new Cell(1, 2).Add(new Paragraph("Геометрические параметры канала").SetTextAlignment(TextAlignment.CENTER)));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Длина, м")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.L.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Ширина, м")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.W.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Глубина, м")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.H.ToString())));
+
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Тип материала")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.MaterialName)));
+
+//            initialTable.AddCell(new Cell(1, 2).Add(new Paragraph("Параметры свойств материала").SetTextAlignment(TextAlignment.CENTER)));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Плотность кг/м³")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.P.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Удельная теплоёмкость, Дж/(кг·°С)")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.C.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Температура плавления, °С")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.T0.ToString())));
+
+//            initialTable.AddCell(new Cell(1, 2).Add(new Paragraph("Режимные параметры процесса").SetTextAlignment(TextAlignment.CENTER)));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Скорость движения крышки, м/с")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.Vu.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Температура крышки, °С")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.Tu.ToString())));
+
+//            initialTable.AddCell(new Cell(1, 2).Add(new Paragraph("Параметры математической модели").SetTextAlignment(TextAlignment.CENTER)));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Шаг расчёта по длине канала, м")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.Step.ToString())));
+
+//            initialTable.AddCell(new Cell(1, 2).Add(new Paragraph("Эмпирические коэффициенты математической модели")
+//                                                        .SetTextAlignment(TextAlignment.CENTER)));
+
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Коэффициент консистенции при температуре приведения, Па·сⁿ")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.U0.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Температурный коэффициент вязкости материала, 1/°С")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.B.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Температура приведения, °С")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.Tr.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Индекс течения материала")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.N.ToString())));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph("Коэффициент теплоотдачи от крышки канала к материалу, Вт/(м²·°С)")));
+//            initialTable.AddCell(new Cell(1, 1).Add(new Paragraph(mathModel.Cp.Au.ToString())));
+
+//            document.Add(initialTable);
+//            document.Add(new AreaBreak());
+
+//            document.Add(new Paragraph("График температуры"));
+//            document.Add(tGraphImage);
+
+//            var nGraphImage = CreateAndFitImage(nBitMap, document);
+//            document.Add(new AreaBreak());
+//            document.Add(new Paragraph("График вязкости"));
+//            document.Add(nGraphImage);
+
+//            document.Add(new Paragraph("Критериальные показатели"));
+//            document.Add(new Paragraph($"Температура продукта {results.T} °С"));
+//            document.Add(new Paragraph($"Вязкость продукта {results.N} Па·с"));
+//            document.Add(new Paragraph($"Производительность канала {results.Q} кг/ч"));
+
+//            document.Add(new AreaBreak());
+
+//            var resultTable = new Table(UnitValue.CreatePercentArray(3)).UseAllAvailableWidth();
+//            resultTable.AddHeaderCell("Координата по длине канала, м");
+//            resultTable.AddHeaderCell("Температура, °С");
+//            resultTable.AddHeaderCell("Вязкость, Па·с");
+
+//            for (var i = 0; i < results.CordTempNs.Count; i++)
+//            {
+//                resultTable.AddCell(results.CordTempNs[i].Cord.ToString());
+//                resultTable.AddCell(results.CordTempNs[i].N.ToString());
+//                resultTable.AddCell(results.CordTempNs[i].Temp.ToString());
+//            }
+
+//            document.Add(new Paragraph("Таблица параметров состояния"));
+//            document.Add(resultTable);
+//            document.Close();
+//        }
+//    }
+//}
+
--- VKR/ViewModel/TechnologistWindowVM.cs
+++ VKR/ViewModel/TechnologistWindowVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using VKR.Utils;
 
 namespace VKR.ViewModel
@@ -16,38 +17,87 @@ namespace VKR.ViewModel
         #endregion
 
         #region Properties
-        private string _displayedImagePath;
-        public string DisplayedImagePath 
+        private string _originalImagePath;
+        public string OriginalImagePath 
         { 
             get 
             { 
-                return _displayedImagePath; 
+                return _originalImagePath; 
             } 
             set 
-            { 
-                _displayedImagePath = value; 
+            {
+                _originalImagePath = value; 
                 OnPropertyChanged();
             } 
         }
+
+        private string _resultImagePath;
+        public string ResultImagePath
+        {
+            get
+            {
+                return _resultImagePath;
+            }
+            set
+            {
+                _resultImagePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _resultMessage;
+        public string ResultMessage
+        {
+            get 
+            { 
+                return _resultMessage; 
+            }
+            set 
+            { 
+                _resultMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
         #endregion
 
         #region Commands
-        private RelayCommand _changePathImage;
+        private RelayCommand _changeImagePath;
 
-        public RelayCommand ChangePathImageCommand
+        public RelayCommand ChangeImagePathCommand
         { 
             get 
             {
-                return _changePathImage ??= new RelayCommand(_ =>
+                return _changeImagePath ??= new RelayCommand(_ =>
                 {
                     OpenFileDialog open = new OpenFileDialog();
                     open.DefaultExt = (".png");
-                    open.Filter = "Pictures (*.jpg;*.gif;*.png)|*.jpg;*.gif;*.png";
+                    open.Filter = "Pictures (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
 
                     if (open.ShowDialog() == true)
-                        DisplayedImagePath = open.FileName;
+                        OriginalImagePath = open.FileName;
                 });
             } 
+        }
+
+        private RelayCommand _showInfo;
+
+        public RelayCommand ShowInfo
+        {
+            get
+            {
+                return _showInfo ??= new RelayCommand(_ =>
+                {
+                    HandyControl.Controls.MessageBox.Show(
+                                    "Данный программный комплекс позволяет обработать входное изображение среза мясной продукции" +
+                                    "для обнаружения фальсификата, по ряду его характеристик, такие как форма, цвет, размер.\n" +
+                                    "В программе реализована функция формирования отчёта в файл формата PDF.\n" +
+                                    "Автор:  Ермаков Даниил Игоревич\n" +
+                                    "Группа: 494\n" +
+                                    "Учебное заведение: СПбГТИ (ТУ)", "Справка о программе",
+                                    MessageBoxButton.OK, MessageBoxImage.Information);
+                });
+            }
         }
         #endregion
     }
