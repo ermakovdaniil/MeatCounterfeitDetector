7a760302c61e328763efd35f3ad890c4ac8527af
Danya <ermakov1709@mail.ru>
2023-01-18 01:23:21 +0300

    Попытка в SIFT через EmguCV

--- VKR/Utils/ImageAnalyzis/ImageAnalyzerStub.cs
+++ VKR/Utils/ImageAnalyzis/ImageAnalyzerStub.cs
@@ -1,13 +1,28 @@
 ﻿using System;
-
 using DataAccess.Models;
-
 
 namespace VKR.Utils.ImageAnalyzis;
 
 public class ImageAnalyzerStub : IImageAnalyzer
 {
-    // наверное лучше стоит передавать битмап вместо пути, но тут хз
+    private void runPythonScript()
+    {
+        //int number = 5;
+        //ScriptEngine engine = Python.CreateEngine();
+        //ScriptScope scope = engine.CreateScope();
+        //engine.ExecuteFile("Test.py", scope);
+
+        //int y = 22;
+        //dynamic x = scope.GetVariable("x");
+        //dynamic z = scope.GetVariable("z");
+        //Console.WriteLine($"{x} + {y} = {z}");
+
+        //dynamic square = scope.GetVariable("square");
+        //// Вызываем функцию и получаем результат
+        //dynamic result = square(number);
+
+    }
+
     public Result analyze(string pathToOrig, User user, double precentOfSimilarity)
     {
         var tempOrigPath = new OriginalPath
---
+++ VKR/Utils/ImageAnalyzis/SIFT.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace VKR.Utils.ImageAnalyzis
+{
+    public static class SIFTT
+    {
+        public static void FindMatch(Mat modelImage, Mat observedImage, out long matchTime, out VectorOfKeyPoint modelKeyPoints, out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat homography)
+        {
+            int k = 2;
+            double uniquenessThreshold = 0.8;
+
+            Stopwatch watch;
+            homography = null;
+
+            modelKeyPoints = new VectorOfKeyPoint();
+            observedKeyPoints = new VectorOfKeyPoint();
+
+            //using (UMat uModelImage = modelImage.ToUMat(AccessType.Read))
+            //using (UMat uObservedImage = observedImage.ToUMat(AccessType.Read))
+            {
+                SIFT sift = new SIFT();
+                //extract features from the object image
+                UMat modelDescriptors = new UMat();
+                // БЫЛО uModelImage
+                sift.DetectAndCompute(modelImage, null, modelKeyPoints, modelDescriptors, false);
+
+                watch = Stopwatch.StartNew();
+
+                // extract features from the observed image
+                UMat observedDescriptors = new UMat();
+                // БЫЛО uObservedImage
+                sift.DetectAndCompute(observedImage, null, observedKeyPoints, observedDescriptors, false);
+                BFMatcher matcher = new BFMatcher(DistanceType.L2);
+                matcher.Add(modelDescriptors);
+
+                matcher.KnnMatch(observedDescriptors, matches, k, null);
+                mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                mask.SetTo(new MCvScalar(255));
+                Features2DToolbox.VoteForUniqueness(matches, uniquenessThreshold, mask);
+
+                int nonZeroCount = CvInvoke.CountNonZero(mask);
+                if (nonZeroCount >= 4)
+                {
+                    nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints,
+                       matches, mask, 1.5, 20);
+                    if (nonZeroCount >= 4)
+                        homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints,
+                           observedKeyPoints, matches, mask, 2);
+                }
+
+                watch.Stop();
+
+            }
+            matchTime = watch.ElapsedMilliseconds;
+        }
+
+        public static Mat Draw(Mat modelImage, Mat observedImage, out long matchTime)
+        {
+            Mat homography;
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            using (VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch())
+            {
+                Mat mask;
+                FindMatch(modelImage, observedImage, out matchTime, out modelKeyPoints, out observedKeyPoints, matches,
+                   out mask, out homography);
+
+                //Draw the matched keypoints
+                Mat result = new Mat();
+                Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+                   matches, result, new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 255), mask);
+
+                #region draw the projected region on the image
+
+                if (homography != null)
+                {
+                    //draw a rectangle along the projected model
+                    Rectangle rect = new Rectangle(Point.Empty, modelImage.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                  new PointF(rect.Left, rect.Bottom),
+                  new PointF(rect.Right, rect.Bottom),
+                  new PointF(rect.Right, rect.Top),
+                  new PointF(rect.Left, rect.Top)
+                    };
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+                    using (VectorOfPoint vp = new VectorOfPoint(points))
+                    {
+                        CvInvoke.Polylines(result, vp, true, new MCvScalar(255, 0, 0, 255), 5);
+                    }
+
+                }
+
+                #endregion
+
+                return result;
+            }
+        }
+    }
+}
