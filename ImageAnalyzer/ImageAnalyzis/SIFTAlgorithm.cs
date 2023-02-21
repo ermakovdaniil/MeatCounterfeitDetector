abbf717e224b103f2d6b1a0627ae5e1bd985848c
Danya <ermakov1709@mail.ru>
2023-02-21 17:49:35 +0300

    Расчёт оценки.

--- ImageAnalyzer/ImageAnalyzis/SIFTAlgorithm.cs
+++ ImageAnalyzer/ImageAnalyzis/SIFTAlgorithm.cs
@@ -31,111 +31,7 @@ namespace ImageAnalyzis
                 Mat mask;
                 FindMatch(modelImage, observedImage, out observedKeyPoints, matches, out mask, out homography);
 
-                //double numberOfKeypoints;
-                //if (modelKeyPoints.Length <= observedKeyPoints.Length)
-                //{
-                //    numberOfKeypoints = (double)modelKeyPoints.Length;
-                //}
-                //else
-                //{
-                //    numberOfKeypoints = (double)observedKeyPoints.Length;
-                //}
-
-                //VectorOfVectorOfDMatch goodMatches = new VectorOfVectorOfDMatch();
-                double distMatches = 0;
-
-                //List<MKeyPoint> modelMatched = new List<MKeyPoint>();
-                //List<MKeyPoint> observedMatched = new List<MKeyPoint>();
-
-                for (int i = 0; i < matches.Size; i++)
-                {
-                    //var arrayOfMatches = matches[i].ToArray();
-                    //if (arrayOfMatches[0].Distance < 0.8 * arrayOfMatches[1].Distance)
-                    //{
-                    //    modelMatched.Add(modelKeyPoints[arrayOfMatches[0].QueryIdx]);
-                    //    observedMatched.Add(observedKeyPoints[arrayOfMatches[1].TrainIdx]);
-                    //}
-
-                    var arrayOfMatches = matches[i].ToArray();
-                    if (arrayOfMatches[0].Distance < 0.8 * arrayOfMatches[1].Distance)
-                    {
-                        //VectorOfDMatch tempVec = new VectorOfDMatch();
-                        //tempVec.Push(arrayOfMatches);
-                        //goodMatches.Push(tempVec);
-                        distMatches++;
-                    }
-                }
-
-                /*
-                //List<MKeyPoint> modelInliners = new List<MKeyPoint>();
-                //List<MKeyPoint> observedInliners = new List<MKeyPoint>();
-                //double inlinerThreshold = 2.5;
-                //for (int i = 0; i < modelMatched.Count; i++)
-                //{
-                    //col = np.ones((3, 1), dtype = np.float64)
-                    //double[,,] col = new double[1, 1, 1];
-                    
-                    //col[0:2, 0] = m.pt
-                    //col[0, 0, 0] = modelMatched[i].Point.X;
-                    //col[0, 0, 1] = modelMatched[i].Point.Y;
-                    //*
-                    //two_d[0:2, 1:] → Select elements from row 0 to row2(excluded) and column 1 till the last column.
-                    //0:2 → Slice for x - axis → select row 0 till row 2(excluded)
-                    //1: → Slice for y - axis → select column 1 till the last column
-
-                    //# Project from image 1 to image 2
-                    //col = np.dot(homography, col)
-                    //col = np.dot();
-
-                    //col /= col[2, 0]
-
-                    //# Calculate euclidean distance
-                    //dist = sqrt(pow(col[0, 0] - matched2[i].pt[0], 2) + pow(col[1, 0] - matched2[i].pt[1], 2))
-
-                    //if dist < inlier_threshold: 
-                    //if(distMatches < inlinerThreshold)
-                    //{
-                        //good_matches.append(cv.DMatch(len(inliers1), len(inliers2), 0))
-
-                        //inliers1.append(matched1[i])
-
-                        //inliers2.append(matched2[i])
-                    //}
-
-                //    var arrayOfMatches = matches[i].ToArray();
-                //    if (arrayOfMatches[0].Distance < 0.8 * arrayOfMatches[1].Distance)
-                //    {
-                //        modelMatched.Add(modelKeyPoints[arrayOfMatches[0].QueryIdx]);
-                //        observedMatched.Add(observedKeyPoints[arrayOfMatches[1].TrainIdx]);
-                //    }
-                //}
-                */
-
-                double goodMatches = CountHowManyPairsExist(mask);
-
-                //var temp = (double)mask.GetData().Length - goodMatches;
-                //score = goodMatches / (temp / 3.0)  * 100.0;
-
-                score = goodMatches / distMatches * 100.0;
-
-                //score = ((double)matches.Length / numberOfKeypoints) * 100.0;
-
-                //score = (goodMatches / (double)matches.Length) * 100.0;
-
-                score = Math.Round(score, 2);
-
-                //if (score > percentOfSimilarity)
-                //{
-                //    Mat scoreImg = new Mat();
-                //    double minVal = Double.MaxValue;
-                //    score = Double.MinValue;
-                //    var minLoc = new Point();
-                //    var maxLoc = new Point();
-                //    CvInvoke.MatchTemplate(modelImage, observedImage, scoreImg, TemplateMatchingType.CcoeffNormed);
-                //    CvInvoke.MinMaxLoc(scoreImg, ref minVal, ref score, ref minLoc, ref maxLoc);
-                //    score *= 100;
-                //    score = Math.Round(score, 2);
-                //}
+                CalculateScore(matches, mask, out score, modelImage, observedImage);
 
                 Mat result = new Mat();
                 if (score > percentOfSimilarity)
@@ -170,7 +66,115 @@ namespace ImageAnalyzis
             }
         }
 
-        public static void FindMatch(Mat modelImage, Mat observedImage, out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat homography)
+        private static void CalculateScore(VectorOfVectorOfDMatch matches, Mat mask, out double score, Mat modelImage, Mat observedImage)
+        {
+            //double numberOfKeypoints;
+            //if (modelKeyPoints.Length <= observedKeyPoints.Length)
+            //{
+            //    numberOfKeypoints = (double)modelKeyPoints.Length;
+            //}
+            //else
+            //{
+            //    numberOfKeypoints = (double)observedKeyPoints.Length;
+            //}
+            //VectorOfVectorOfDMatch goodMatches = new VectorOfVectorOfDMatch();
+            //List<MKeyPoint> modelMatched = new List<MKeyPoint>();
+            //List<MKeyPoint> observedMatched = new List<MKeyPoint>();
+            //List<MKeyPoint> modelInliners = new List<MKeyPoint>();
+            //List<MKeyPoint> observedInliners = new List<MKeyPoint>();
+            //double inlinerThreshold = 2.5;
+            //for (int i = 0; i < modelMatched.Count; i++)
+            //{
+                //col = np.ones((3, 1), dtype = np.float64)
+                //double[,,] col = new double[1, 1, 1];
+
+                //col[0:2, 0] = m.pt
+                //col[0, 0, 0] = modelMatched[i].Point.X;
+                //col[0, 0, 1] = modelMatched[i].Point.Y;
+                //*
+                //two_d[0:2, 1:] → Select elements from row 0 to row2(excluded) and column 1 till the last column.
+                //0:2 → Slice for x - axis → select row 0 till row 2(excluded)
+                //1: → Slice for y - axis → select column 1 till the last column
+
+                //# Project from image 1 to image 2
+                //col = np.dot(homography, col)
+                //col = np.dot();
+
+                //col /= col[2, 0]
+
+                //# Calculate euclidean distance
+                //dist = sqrt(pow(col[0, 0] - matched2[i].pt[0], 2) + pow(col[1, 0] - matched2[i].pt[1], 2))
+
+                //if dist < inlier_threshold: 
+                //if(distMatches < inlinerThreshold)
+                //{
+                    //good_matches.append(cv.DMatch(len(inliers1), len(inliers2), 0))
+
+                    //inliers1.append(matched1[i])
+
+                    //inliers2.append(matched2[i])
+                //}
+
+            //    var arrayOfMatches = matches[i].ToArray();
+            //    if (arrayOfMatches[0].Distance < 0.8 * arrayOfMatches[1].Distance)
+            //    {
+            //        modelMatched.Add(modelKeyPoints[arrayOfMatches[0].QueryIdx]);
+            //        observedMatched.Add(observedKeyPoints[arrayOfMatches[1].TrainIdx]);
+            //    }
+            //}
+            
+            double distMatches = 0;
+            for (int i = 0; i < matches.Size; i++)
+            {
+                var arrayOfMatches = matches[i].ToArray();
+                if (arrayOfMatches[0].Distance < 0.8 * arrayOfMatches[1].Distance)
+                {
+                    //VectorOfDMatch tempVec = new VectorOfDMatch();
+                    //tempVec.Push(arrayOfMatches);
+                    //goodMatches.Push(tempVec);
+
+                    //modelMatched.Add(modelKeyPoints[arrayOfMatches[0].QueryIdx]);
+                    //observedMatched.Add(observedKeyPoints[arrayOfMatches[1].TrainIdx]);
+
+                    distMatches++;
+                }
+            }
+
+            double goodMatches = CountHowManyPairsExist(mask);
+
+            //var temp = (double)mask.GetData().Length - goodMatches;
+            //score = goodMatches / (temp / 3.0)  * 100.0;
+
+            double fisrtScore = goodMatches / (goodMatches + 15);
+
+            //score = ((double)matches.Length / numberOfKeypoints) * 100.0;
+
+            //score = (goodMatches / (double)matches.Length) * 100.0;
+
+
+            Mat scoreImg = new Mat();
+            double minVal = Double.MaxValue;
+            double secondScore = Double.MinValue;
+            var minLoc = new Point();
+            var maxLoc = new Point();
+            CvInvoke.MatchTemplate(modelImage, observedImage, scoreImg, TemplateMatchingType.CcoeffNormed);
+            CvInvoke.MinMaxLoc(scoreImg, ref minVal, ref secondScore, ref minLoc, ref maxLoc);
+
+
+            if(fisrtScore > secondScore)
+            {
+                score = fisrtScore;
+            }
+            else
+            {
+                score = secondScore;
+            }
+
+            score *= 100;
+            score = Math.Round(score, 2);
+        }
+
+        private static void FindMatch(Mat modelImage, Mat observedImage, out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat homography)
         {
             int k = 2;
             double uniquenessThreshold = 0.8;
@@ -210,7 +214,7 @@ namespace ImageAnalyzis
             }
         }
 
-        public static double CountHowManyPairsExist(Mat mask)
+        private static double CountHowManyPairsExist(Mat mask)
         {
             var matched = mask.GetData();
             var list = matched.OfType<byte>().ToList();
