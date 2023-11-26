4961f9812d096270531bfe868b666e8024708069
Danya <ermakov1709@mail.ru>
2023-11-27 01:20:44 +0300

    backup

--- backup_script.bat
+++ backup_script.bat
@@ -2,8 +2,8 @@
 set TIMESTAMP=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 set FILENAME=C:\Users\Даня\source\repos\MeatCountefeitDetector\Backup-%TIMESTAMP%.sql
 
-pg_dump --username=postgres --dbname=CounterfeitKB --clean --create --file="%FILENAME%"
+pg_dump --username=postgres --dbname=CounterfeitKB --create --file="%FILENAME%"
 
 set FILENAME=C:\Users\Даня\source\repos\MeatCountefeitDetector\Backup-%TIMESTAMP%.sql
 
-pg_dump --username=postgres --dbname=ResultDB --clean --create --file="%FILENAME%"
+pg_dump --username=postgres --dbname=ResultDB --create --file="%FILENAME%"
